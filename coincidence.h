dc88b294ad68e3025e470eda548e9a4dde840042
fwhelan <fwhelan@localhost>
2018-05-14 14:53:01 +0100

    working code

--- coincidence.h
+++ coincidence.h
@@ -23,10 +23,10 @@ class Coincidence
     private:
         static std::map<std::pair<std::string, std::string>, double> calc_phylogenetic_distances();
 	//static std::vector<double> calculate_phylogenetic_distance(const std::map<const Beta*, int>& edgeList_B1, const std::map<const Beta*, int>& edgeList_B2);
-	static double pull_phylogenetic_maximum(  std::map<std::pair<std::string, std::string>, double> phylo_dists, std::vector<std::string> edges_ovlp);
+	static double pull_phylogenetic_maximum(  const std::map<std::pair<std::string, std::string>, double>& phylo_dists, const std::map<const Beta*, int>& edges_yain, const std::map<const Beta*, int>& edges_tain);
 	static std::vector<double> calculate_phylogenetic_distance(std::vector<std::string> edges_ovlp);
 	static std::vector<int> calculate_syntentic_distance(const std::map<const Beta*, int>& edgeList_B1, const std::map<const Beta*, int>& edgeList_B2, const id_lookup<Edge>& edge_table, const Alpha& alpha_yain, const Alpha& alpha_tain);
-	static void _coincidence_to_p( const DataSet& dataset, const Alpha& alpha_yain, const Alpha& alpha_tain, double cor_sig_level, int both_of, int one_of, int any_yain, int any_tain, double max_phylodist, double avg_syndist );
+	static void _coincidence_to_p( const DataSet& dataset, const Alpha& alpha_yain, const Alpha& alpha_tain, double cor_sig_level, int both_of, int one_of, int any_yain, int any_tain, const std::map<std::pair<std::string, std::string>, double>& phylo_dists, const std::map<const Beta*, int>& edges_yain, const std::map<const Beta*, int>& edges_tain, const id_lookup<Edge>& edge_table, double max_act_phylodist);
         static void _write_header();
 };
 
