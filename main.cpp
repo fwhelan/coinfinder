29031d41b7755e856b555602d0020c628405fbbc
Fiona J Whelan <3144468+fwhelan@users.noreply.github.com>
2018-07-26 20:18:41 +0100

    v1.1; -h now produces a help page

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 #include "parameters.h"
 #include "connectivity.h"
 #include "test_cases.h"
-
+#include <stdio.h>
+#include <string.h>
 
 /**
  * Entry point, boom!
@@ -33,6 +34,59 @@ int main( int argc, const char** argv )
         std::cerr << "Please see readme.md for details." << std::endl;
 
         return 1;
+    }
+
+    //
+    // Check for ask for help
+    //
+    //char *help = NULL;
+    //help = strstr(argv, "-h");
+    //if (help) {
+	//std::cerr << "Hello world" << std::endl;
+    //}
+    std::string help = "-h";
+    for (int n = 1; n < argc; ++n) {
+        std::string arg = argv[ n ];
+	if (arg == help) {
+		std::cerr << "./confinder [OPTIONS]" << std::endl;
+		std::cerr << "File input- specify either: " << std::endl;
+		std::cerr << "    -a or --a              The path to the Gamma-to-Alpha family file with (gamma)(TAB)(alpha)" << std::endl;
+		std::cerr << "    -b or --b              The path to the Gamma-to-Beta family file with (gamma)(TAB)(beta)" << std::endl;
+
+		std::cerr << "  or" << std::endl;
+		std::cerr << "    -d or --d              The path of the Alpha-to-Beta file with (alpha)(TAB)(beta)" << std::endl;
+		std::cerr << "Significance- specify: " << std::endl;
+		std::cerr << "    -L or --level          Specify the significnace level cutoff" << std::endl;
+		std::cerr << "Naming (optional): " << std::endl;
+		std::cerr << "    -A or --alpha          Specify the name of the Alpha group" << std::endl;
+		std::cerr << "    -B or --beta           Specify the name of the Beta group" << std::endl;
+		std::cerr << "    -C or --gamma          Specify the name of the Gamma group" << std::endl;
+		std::cerr << "Mode- specify: " << std::endl;
+		std::cerr << "    -e or --connectivity   Connectivity analysis" << std::endl;
+		std::cerr << "    -c or --coincidence    Coincidence analysis" << std::endl;
+		std::cerr << "Significance correction- specify: " << std::endl;
+		std::cerr << "    -n or --nocorrection   No correction, use value as-is" << std::endl;
+		std::cerr << "    -m or --bonferroni     Bonferroni correction multiple correction" << std::endl;
+		std::cerr << "    -r or --fraction       (Connectivity analysis only) Use fraction rather than p-value" << std::endl;
+		std::cerr << "Alternative hypothesis- specify: " << std::endl;
+		std::cerr << "    -g or --greater        Greater (recommended)" << std::endl;
+		std::cerr << "    -l or --less           Less" << std::endl;
+		std::cerr << "    -t or --twotailed      Two-tailed" << std::endl;
+		std::cerr << "Set mode (mandatory for coincidence analysis):" << std::endl;
+		std::cerr << "    -f or --full           Full; consider all data." << std::endl;
+		std::cerr << "    -u or --union          Union; only consider the edges which pertain to the groups being analysed." << std::endl;
+		std::cerr << "Max mode (mandatory for coincidence analysis):" << std::endl;
+		std::cerr << "    -o or --accompany      Overlap; identify groups that tend to coincide." << std::endl;
+		std::cerr << "    -s or --avoid          Separation; identify groups that tend to avoid." << std::endl;
+		std::cerr << "Miscellaneous:" << std::endl;
+		std::cerr << "    -p or --phylogeny      Phylogeny of Betas in Newick format (mandatory)" << std::endl;
+		std::cerr << "    -v or --verbose        Verbose output." << std::endl;
+		std::cerr << "    -i or --filter         Permit filtering of saturated and low-abundance data." << std::endl;
+		std::cerr << "    -q or --query          Query a specific Alpha family." << std::endl;
+		std::cerr << "    -T or --test           Runs the test cases and exits." << std::endl;
+		std::cerr << "    -E or --all            Outputs all results, regardless of significance." << std::endl;
+		return 1;
+	}
     }
 
     //
