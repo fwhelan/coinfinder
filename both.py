fdcaee27641f435cd185eaa150e8942d15289988
fwhelan <fwhelan@localhost>
2018-08-08 16:52:05 +0100

    combinding python code into one file for potential cross platform bug fix

--- both.py
+++ both.py
@@ -37,3 +37,13 @@ def calc( *thelist ):
 			for nodeB in edges[nodeA]:
 				dists.extend((nodeA,nodeB,edges[nodeA][nodeB]))
 	return(dists)
+def calc1( *thelist ):
+        phylo = ete3.Tree(str(thelist[0]), format=8)
+        edges_union = thelist[1:]
+        #Find the common ancestor internal node in phylo of the edges in edges_union and calculate its distance to the root node
+        ancestor = phylo.get_common_ancestor(edges_union)
+        dist = ancestor.get_distance(phylo.get_tree_root())
+        name = ancestor.name
+        if (ancestor.is_root()):
+                return("Root")
+        return(str(name))
